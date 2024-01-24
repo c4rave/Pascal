a0ddad7082fc6d89f2e98b013b38dd4e5f83bcf0
Rave <vela.santiagorafael@gmail.com>
2024-01-24 09:04:54 -0600

    Se hicieron pequeñas correciones de sintaxis y ortografía

--- División/Division.pas
+++ División/Division.pas
@@ -4,7 +4,7 @@ var
  m,n,a:real;
 begin
   clrscr;
-  write ('Programa para dividir 2 numeros, por Rafael Vela;');
+  writeln ('Programa para dividir 2 numeros, por Rafael Vela;');
   write ('Introduce el primer numero ');
   read (m);
   write ('Introduce el segundo numero ');
