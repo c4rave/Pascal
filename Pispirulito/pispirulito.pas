27c7f3e27994bc5c86a44bb76beded07a2049db7
Rave <139708427+c4rave@users.noreply.github.com>
2024-01-24 12:45:52 -0600

    Trabajos añadidos del 24 de enero
    Programa hecho en clase

---
+++ Pispirulito/pispirulito.pas
@@ -0,0 +1,19 @@
+program pispirulito;
+uses crt;
+var
+    c:real;
+const
+    p = 179;
+    r = 205;
+    e = 98;
+    g = 180;
+    l = 199;
+begin
+    clrscr;
+    writeln('Programa para ayudar a Pispirulito con la cuenta');
+    writeln('Programado por Rafael Vela');
+    writeln(' ');
+    c:= p+r+e+g+l;
+    writeln('Pispirulito y cada uno de sus cuates pagara: $',c:4:2);
+    readkey;
+end.
