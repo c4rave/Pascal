b0b55a745ae55dbcd40abde6a8704b2be65a81d2
Rave <139708427+c4rave@users.noreply.github.com>
2024-01-30 12:50:46 -0600

    Promedio actualizado con 5 calificaciones

--- Promedio/promedio.pas
+++ Promedio/promedio.pas
@@ -1,18 +1,22 @@
 program promedio;
 uses crt;
 var
-u,d,t,p,k:real;
+u,d,t,a,c,p,k:real; (*Cada variable es para una calificación distinta*)
 begin
     clrscr;
     writeln('Programa para calcular promedio por Rafael Vela');
-    writeln('Ingresa la calificacion del primer parcial');
+    writeln('Ingresa la primera calificacion ');
     read(u);
-    writeln('Ingresa la calificacion del segundo parcial');
+    writeln('Ingresa la segunda calificacion ');
     read(d);
-    writeln('Ingresa la calificacion del tercer parcial');
+    writeln('Ingresa la tercera calificacion ');
     read(t);
-    k:=u+d+t;
-    p:=k/3;
+    writeln('Ingresa la cuarta calificacion ');
+    read(a);
+    writeln('Ingresa la quinta calificacion ');
+    read(c);
+    k:=u+d+t+a+c;
+    p:=k/5;
     writeln('Tu promedio es: ',p:2:2);
 readkey;
 end.
