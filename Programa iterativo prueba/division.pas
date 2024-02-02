41219827cd4bc9915972d56665d7be0353c5fdf8
Rave <139708427+c4rave@users.noreply.github.com>
2024-02-02 12:54:56 -0600

    Programa iterarivo comentado

--- Programa iterativo prueba/division.pas
+++ Programa iterativo prueba/division.pas
@@ -8,19 +8,19 @@ const
     a=2023;
 begin   
     clrscr;
-    repeat
+    repeat (*Si la respuesta de la linea 22 es s, se ejecuta el programa nuevamente desde acá*)
     begin
-       textcolor(green);
+       textcolor(green); (*usa la libreria CRT, para manejo de pantalla, damos color al texto*)
        writeln('');
        writeln('');
        writeln('');
-       Textcolor(green);
+       Textcolor(green); (*usa la libreria CRT, para manejo de pantalla, damos color al texto*)
        Write('Escribe el año en que naciste: ');
        Readln(num1);
        total:=a-num1;
        writeln('Tu edad es de ', total:0:0, ' años');
-       Writeln('¿Quieres calcular otra edad? (s/n): ');
+       Writeln('¿Quieres calcular otra edad? (s/n): '); (*Lineas 22 y 23 son para repetir el probrama, junto a la linea 11 y 25*)
        Readln(opcion);
     end;
-    until(opcion='n') ;
+    until(opcion='n') ; (*Si la respuesta es n se termina de ejecutar el programa*)
 End.
