5fa17f5793fe861fbf38abbd5c2cb258d6fc9ec8
Rave <139708427+c4rave@users.noreply.github.com>
2024-02-06 13:33:24 -0600

    v2.0

--- División/Division.pas
+++ División/Division.pas
@@ -1,15 +1,22 @@
 program division;
 uses crt;
 var
- m,n,a:real;
+ m,q,a:real;
+ opcion:char;
 begin
   clrscr;
-  writeln ('Programa para dividir 2 numeros, por Rafael Vela;');
-  write ('Introduce el primer numero ');
-  read (m);
-  write ('Introduce el segundo numero ');
-  read (n);
-  a:=m/n;
-  write ('El resultado es ',a:5:2);
-  readln;
+  repeat
+      writeln ('Programa para dividir 2 numeros, por Rafael Vela;');
+      writeln ('Introduce el primer numero ');
+      readln (m);
+      writeln ('Introduce el segundo numero ');
+      readln (q);
+      a:=m/q;
+      writeln ('El resultado es ',a:5:2);
+      writeln('Quieres dividir 2 numeros otra vez? s/n  ');
+      readln(opcion);
+  until (opcion='n');
+  end.
+end.
+
 end.
