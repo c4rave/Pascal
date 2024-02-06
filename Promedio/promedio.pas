412b37da2f1bfd01146aff7873e4704086406f08
Rave <139708427+c4rave@users.noreply.github.com>
2024-02-06 13:13:24 -0600

    v2.0

--- Promedio/promedio.pas
+++ Promedio/promedio.pas
@@ -1,22 +1,26 @@
 program promedio;
-uses crt;
+uses crt,math;
 var
 u,d,t,a,c,p,k:real; (*Cada variable es para una calificación distinta*)
+opcion:char; (*para repetir el programa*)
 begin
     clrscr;
-    writeln('Programa para calcular promedio por Rafael Vela');
-    writeln('Ingresa la primera calificacion ');
-    read(u);
-    writeln('Ingresa la segunda calificacion ');
-    read(d);
-    writeln('Ingresa la tercera calificacion ');
-    read(t);
-    writeln('Ingresa la cuarta calificacion ');
-    read(a);
-    writeln('Ingresa la quinta calificacion ');
-    read(c);
-    k:=u+d+t+a+c;
-    p:=k/5;
-    writeln('Tu promedio es: ',p:2:2);
-readkey;
+    repeat
+        writeln('Programa para calcular promedio por Rafael Vela');
+        writeln('Ingresa la primera calificacion ');
+        readln(u);
+        writeln('Ingresa la segunda calificacion ');
+        readln(d);
+        writeln('Ingresa la tercera calificacion ');
+        readln(t);
+        writeln('Ingresa la cuarta calificacion ');
+        readln(a);
+        writeln('Ingresa la quinta calificacion ');
+        readln(c);
+        k:=u+d+t+a+c;
+        p:=k/5;
+        writeln('Tu promedio es: ',p:2:2);
+        writeln('Quieres sacar otro promedio? s/n  ');
+        readln(opcion);
+    until(opcion='n');
 end.
