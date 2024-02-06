0eac8df3352cd20a1d8285dec41d3b2b85eb27f8
Rave <139708427+c4rave@users.noreply.github.com>
2024-02-06 13:24:25 -0600

    v2.0

--- Manzanas/manzanas.pas
+++ Manzanas/manzanas.pas
@@ -2,16 +2,21 @@ program manzanas;
 uses crt;
 const
   c = 25;
-  m = 5000;
 var
+  m:integer;
   t:real;
+  opcion:char;
 begin
-
   clrscr;
-  writeln ('Programa para ayudar al granjero por Rafael Vela, ');
-  writeln ('Tenemos 500 manzanas, entonces...' );
-  t:=m/c;
-  writeln ('Necesitas ',t:5:2,' cajas');
-  readln;
-
+  repeat
+    writeln ('Programa para ayudar al granjero por Rafael Vela, ');
+    writeln ('Cuantas manzanas tenemos?');
+    readln (m);
+    writeln ('Tenemos ',m, ' manzanas, entonces...' );
+    t:=m/c;
+    writeln ('Necesitas ',t:5:1,' cajas');
+    writeln ('Quieres volver a ver cuantas cajas necesitas? s/n ');
+    readln(opcion);
+  until(opcion='n');
+  end.
 end.
